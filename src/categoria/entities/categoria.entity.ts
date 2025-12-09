edca4a4e0ca031ed1bca3a89a35ee120639f887d
Mamadou DIAGNE <diagne264@gmail.com>
2025-12-09 14:01:00 -0300

    feat: cria entidades categoria e produto inicial

--- src/categoria/entities/categoria.entity.ts
+++ src/categoria/entities/categoria.entity.ts
@@ -1,1 +1,18 @@
-export class Categoria {}
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { Produto } from '../../produto/entities/produto.entity';
+
+@Entity('tb_categorias')
+export class Categoria {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 100, nullable: false })
+  nome: string;
+
+  @Column({ length: 255 })
+  descricao: string;
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  @OneToMany(() => Produto, (produto) => produto.categoria)
+  produtos: Produto[];
+}
--- src/produto/entities/produto.entity.ts
+++ src/produto/entities/produto.entity.ts
@@ -1,1 +1,23 @@
-export class Produto {}
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { Categoria } from '../../categoria/entities/categoria.entity';
+
+@Entity('tb_produtos')
+export class Produto {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 100, nullable: false })
+  titulo: string;
+
+  @Column('decimal', { precision: 10, scale: 2 })
+  preco: number;
+
+  @Column({ type: 'text' })
+  descricao: string;
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  @ManyToOne(() => Categoria, (categoria) => categoria.produtos, {
+    onDelete: 'CASCADE',
+  })
+  categoria: Categoria;
+}
